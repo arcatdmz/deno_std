6ddc2c3b3da78e885d2186aff30735d1356f3ac4
Jun Kato <i@junkato.jp>
2019-05-16 02:31:31 +0900

    Add test file

---
+++ archive/zip_test.ts
@@ -0,0 +1,18 @@
+/**
+ * Zip test
+ */
+import { test, runIfMain } from "../testing/mod.ts";
+import { assertEquals } from "../testing/asserts.ts";
+
+import pako from "./pako/index.js";
+
+test(async function inflateAndDeflate(): Promise<void> {
+  const inputString = "hello tar world!";
+  const input = new TextEncoder().encode(inputString);
+  const inflated = pako.inflate(input)
+  const deflated = pako.deflate(inflated);
+  const outputString = new TextDecoder("utf-8").decode(deflated);
+  assertEquals(inputString, outputString);
+});
+
+runIfMain(import.meta);
