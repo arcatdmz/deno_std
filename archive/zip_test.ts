9835f797acbc90746504a2a1e6bf8c8c9dbecda6
Jun Kato <i@junkato.jp>
2019-05-16 03:17:11 +0900

    Fix test code

--- archive/zip_test.ts
+++ archive/zip_test.ts
@@ -6,12 +6,12 @@ import { assertEquals } from "../testing/asserts.ts";
 
 import pako from "./pako/index.js";
 
-test(async function inflateAndDeflate(): Promise<void> {
-  const inputString = "hello tar world!";
+test(async function deflateAndInflate(): Promise<void> {
+  const inputString = "hello zip world!";
   const input = new TextEncoder().encode(inputString);
-  const inflated = pako.inflate(input);
-  const deflated = pako.deflate(inflated);
-  const outputString = new TextDecoder("utf-8").decode(deflated);
+  const deflated = pako.deflate(input);
+  const inflated = pako.inflate(deflated);
+  const outputString = new TextDecoder("utf-8").decode(inflated);
   assertEquals(inputString, outputString);
 });
 
