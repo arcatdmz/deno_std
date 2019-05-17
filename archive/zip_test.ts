8d7a11174f52e99902a997c80d34ee1cb7ad6f84
Jun Kato <i@junkato.jp>
2019-05-18 00:47:46 +0900

    Add a test for text encoder/decoder

--- archive/zip_test.ts
+++ archive/zip_test.ts
@@ -3,8 +3,11 @@
  */
 import { test, runIfMain } from "../testing/mod.ts";
 import { assertEquals } from "../testing/asserts.ts";
+import { bytesFindIndex } from "../bytes/bytes.ts";
 
 import * as pako from "./pako/index.ts";
+
+import { string2buf, buf2string } from "./pako/lib/utils/strings.ts";
 
 test(async function deflateAndInflate(): Promise<void> {
   const inputString = "hello zip world!";
@@ -13,6 +16,17 @@ test(async function deflateAndInflate(): Promise<void> {
   const inflated = pako.inflate(deflated);
   const outputString = new TextDecoder("utf-8").decode(inflated);
   assertEquals(inputString, outputString);
+});
+
+test(async function stringAndBuffer(): Promise<void> {
+  const inputString = "hello こんにちは";
+  const nativeOutput = new TextEncoder().encode(inputString);
+  const output = string2buf(inputString);
+  const outputString = buf2string(output, output.length);
+  const nativeOutputString = new TextDecoder("utf-8").decode(output);
+  assertEquals(bytesFindIndex(nativeOutput, output), 0);
+  assertEquals(inputString, outputString);
+  assertEquals(outputString, nativeOutputString);
 });
 
 runIfMain(import.meta);
